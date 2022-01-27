3ee9fef8b7fbdfe0298ab3b90a1c55dedb284f16
ozgencdev <ozgencdev@gmail.com>
2022-01-27 10:22:05 +0300

    edit: fetch users from sessions

--- src/js/components/leaderboard.js
+++ src/js/components/leaderboard.js
@@ -1,5 +1,5 @@
 // src/components/pagination.table.js
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 
 import { useTable, usePagination } from "react-table";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -155,6 +155,12 @@ function Table({ columns, data }) {
 }
 
 function LeaderBoard() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const data = sessionStorage.getItem("users");
+    setUsers(JSON.parse(data));
+  }, []);
+
   const columns = React.useMemo(
     () => [
       {
@@ -166,7 +172,7 @@ function LeaderBoard() {
           },
           {
             Header: "Username",
-            accessor: "username",
+            accessor: "id",
           },
           {
             Header: "Score",
@@ -182,169 +188,7 @@ function LeaderBoard() {
     []
   );
 
-  const data = [
-    {
-      rank: 1,
-      username: "John Doe",
-      score: "100",
-      country: "USA",
-    },
-    {
-      firstName: "midnight-wad0y",
-      lastName: "data-7h4xf",
-      age: 1,
-      visits: 56,
-      progress: 15,
-      status: "complicated",
-    },
-    {
-      firstName: "tree-sbdb0",
-      lastName: "friendship-w8535",
-      age: 1,
-      visits: 45,
-      progress: 66,
-      status: "single",
-    },
-    {
-      firstName: "chin-borr8",
-      lastName: "shirt-zox8m",
-      age: 0,
-      visits: 25,
-      progress: 67,
-      status: "complicated",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-  ];
-  console.log(JSON.stringify(data));
-
-  return <Table columns={columns} data={data} />;
+  return <Table columns={columns} data={users} />;
 }
 
 export default LeaderBoard;
